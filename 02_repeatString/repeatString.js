f2afe41e93b3fb83e03d65d193820809991e3d41
Fewtatwo <opogmith@gmail.com>
2023-01-29 11:43:14 +0800

    01-02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
 
+    let input = word;
+    let toReturn = "";
+    
+    if (count < 0) {
+        return toReturn = "ERROR";
+    } else {
+        while (count > 0) {
+            toReturn = toReturn + input;
+            count--;
+        }
+        return toReturn;
+    }
 };
 
 // Do not edit below this line

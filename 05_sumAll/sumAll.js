8637d8ce89c827c9ecb7e6bc5c52cdf6a4baf895
Fewtatwo <opogmith@gmail.com>
2023-01-31 12:59:20 +0800

    complete ex-05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let aInput = typeof a;
+    let bInput = typeof b;
+    
+ 
+    if ((aInput !== 'number' || bInput !== 'number') || a < 0 || b < 0) {
+        return 'ERROR';
+    } else {
+        if (a < b) {
+            for(let i = a; i <= b; i++){
+                sum += i;
+            }
+            return sum;
+        } else {
+            for(let i = b; i <= a; i++){
+                sum += i;
+            }
+            return sum;
+        }
+    }
 
-};
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;

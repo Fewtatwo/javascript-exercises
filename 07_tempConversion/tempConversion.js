5b314cbb696d96400ee73810e299b6c51fe36fa3
Fewtatwo <opogmith@gmail.com>
2023-02-06 17:59:26 +0800

    finish ex-07-08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farValue) {
+  let converted;
+  if (farValue !== 32) {
+      converted = Math.round(((farValue  - 32) / 1.8)*10) / 10;   
+  return converted;
+  } else {
+      return converted = 0;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celValue) {
+  let converted;
+  if (celValue !== 0) {
+      converted = Math.round(((celValue * 1.8) + 32)*10) / 10;
+      return converted;
+  } else {
+      return converted = 32;
+  }
+
+
+
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum;
+  sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let difference;
+  difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  const given = a;
+  let arrSum = 0;
+  
+  given.forEach (elem => {
+      arrSum += elem;
+  });
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const given = a;
+  let arrProd = 1;
+  
+  given.forEach (elem => {
+      arrProd *= elem;
+  });
+  return arrProd;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  const exponent = b;
+  const base = a;
+  let prod = 1 ;
+
+  for (let i = 0; i != exponent; i++) {
+     prod *= base;
+  };
+
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let prod = a;
+  let init = a - 1;
+  
+  if (prod === 0) {
+      return prod = 1;
+  } else {
+      for (let i = init; i != 0; i--) {
+          prod *= i;
+      };
+      return prod;
+  }
 };
 
 // Do not edit below this line

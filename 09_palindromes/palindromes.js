c163a0168c303e8f9b8ce852729dd5775c485a93
Fewtatwo <opogmith@gmail.com>
2023-02-08 09:02:04 +0800

    add ex-09-10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (toReverse) {
+
+    const strArray = ((toReverse.toUpperCase()).replace(/\s/g, '')).split('');
+    const mustRe = ['!', '.', '?', ',', "'"];
+    const newArray = [];
+    
+    strArray.forEach(element => { 
+        if (!mustRe.includes(element)) { 
+                newArray.push(element); 
+        }    
+    } 
+    );
+
+    const origArray =  newArray.join('');
+    newArray.reverse();
+    const outPut = newArray.join('');
+
+    if ( origArray  === outPut ) {
+        return true;
+    } else {
+        return false;
+    };
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(toFib) {
+    let a = 0;
+    let b = 1;
+    let c;
+    const fibArr = [1];
 
+    if (parseInt(toFib) < 0) {
+        return "OOPS";
+    } else {
+        for(let i = 1; i !== parseInt(toFib); i++) {
+            c = a + b;
+            fibArr.push(c);
+            a = b;
+            b = c;
+        };
+         //  return c;
+        return parseInt((fibArr.slice(-1)).join(''));
+    };
 };
 
 // Do not edit below this line

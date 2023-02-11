9f20de09a1778d259b43ea450b8d67374d828d11
Fewtatwo <opogmith@gmail.com>
2023-02-11 11:08:38 +0800

    add ex-11-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce(( prevPersonAge, currPersonAge) => {
+        const oldestAge = getAge(prevPersonAge.yearOfBirth, prevPersonAge.yearOfDeath);
+        const currentAge = getAge(currPersonAge.yearOfBirth, currPersonAge.yearOfDeath);
+        return oldestAge < currentAge ? currPersonAge : prevPersonAge;
+    }); 
+};
 
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
